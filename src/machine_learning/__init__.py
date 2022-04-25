459ace37cdfc743c2e28035098bd1274fda30a38
n0t10n <0ts.notion@gmail.com>
2022-04-25 19:37:39 +0200

    Scaler function done

--- src/machine_learning/__init__.py
+++ src/machine_learning/__init__.py
@@ -1,28 +1,29 @@
 import numpy as np
-import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
 
-def scaler(self, scaler: str, data: np.array):
-    """
-    Scales the data.
+class preprocessing:
+    
+    def scaler(self, scaler: str, data: np.array):
+        """
+        Scales the data.
 
-    Args:
-        scaler: A scaler to choose from sklearn library. StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
-        data: Array like data to be scaled.
-    
-    Returns:
-        The data scaled.
-    """
-    try:
-        if scaler=='StandardScaler':
-            return StandardScaler().fit_transform(data)
-        elif scaler=='MinMaxScaler':
-            return MinMaxScaler().fit_transform(data)
-        elif scaler=='MaxAbsScaler':
-            return MaxAbsScaler().fit_transform(data)
-        elif scaler=='RobustScaler':
-            return RobustScaler().fit_transform(data)
-        else:
-    
-    except ValueError:
-        print('Choose one of the scalers listed.')
+        Args:
+            scaler: A scaler to choose from sklearn library. StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
+            data: Array like data to be scaled.
+        
+        Returns:
+            The data scaled.
+        """
+        try:
+            if scaler=='StandardScaler':
+                return StandardScaler().fit_transform(data)
+            elif scaler=='MinMaxScaler':
+                return MinMaxScaler().fit_transform(data)
+            elif scaler=='MaxAbsScaler':
+                return MaxAbsScaler().fit_transform(data)
+            elif scaler=='RobustScaler':
+                return RobustScaler().fit_transform(data)
+            else:
+        
+        except ValueError:
+            print('Choose one of the scalers listed.')
