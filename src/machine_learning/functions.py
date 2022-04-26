d3e88e863988dd2c87b229ae3f130f7ce0b4e2ac
n0t10n <0ts.notion@gmail.com>
2022-04-26 20:29:16 +0200

    Lucy funciones check!

--- src/machine_learning/functions.py
+++ src/machine_learning/functions.py
@@ -3,66 +3,68 @@ from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error,
 from sklearn.decomposition import PCA
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-from fancyimpute import IterativeImputer as MICE
 from xgboost import XGBRegressor
 import tensorflow as tf
+
 import sys
 import os
-<<<<<<< HEAD
 import urllib.request
 
 from PIL import Image
-=======
->>>>>>> 871628556d48202b1a026bc113c41d0c3e3c3ee1
 import cv2
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def remover_vello(imagen):
+def remover_vello(imagen: np.array):
     '''
     Se aplica una máscara para eliminar el vello en la imagen. La función devuelve una `imagen` con las
     mismas dimensiones que la original.
     
-    Parameters
-    ----------
-    imagen : img
+    Args:
+        imagen: Matríz con valores entre 0 y 255.
 
-    Output
-    ------
-    new_img : img
-
+    Returns:
+        new_img: La imagen (matríz) con los filtros aplicados.
     '''
-    #1. Convertir a escala de grises
-    img_grayScale = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-    # Kernel para el filtrado morfológico
-    kernel = cv2.getStructuringElement(1,(17,17))
-    # Filtrado BlackHat para encontrar los contornos del cabello
-    blackhat = cv2.morphologyEx(img_grayScale, cv2.MORPH_BLACKHAT, kernel)
-    # Intensificar los contornos del cabello en preparación para el algoritmo de pintura
-    _,mask = cv2.threshold(blackhat,12,255,cv2.THRESH_BINARY)
-    # Pintar la imagen original dependiendo de la máscara
-    new_img = cv2.inpaint(imagen,mask,1,cv2.INPAINT_TELEA)
-    return new_img
+    try:
+        #1. Convertir a escala de grises
+        img_grayScale = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+        # Kernel para el filtrado morfológico
+        kernel = cv2.getStructuringElement(1,(17,17))
+        # Filtrado BlackHat para encontrar los contornos del cabello
+        blackhat = cv2.morphologyEx(img_grayScale, cv2.MORPH_BLACKHAT, kernel)
+        # Intensificar los contornos del cabello en preparación para el algoritmo de pintura
+        _,mask = cv2.threshold(blackhat,12,255,cv2.THRESH_BINARY)
+        # Pintar la imagen original dependiendo de la máscara
+        new_img = cv2.inpaint(imagen,mask,1,cv2.INPAINT_TELEA)
+
+        return new_img
+    
+    except:
+        print(f'El formato {imagen} no es el adecuado. Revisa la descripción de la función.')
 
 def mask_fondo(imagen):
     '''
     Función para eliminar el fondo de la imagen.
 
-    Parameters
-    ----------
-    imagen : img
+    Args:
+        imagen: Matríz con valores entre 0 y 255.
 
-    Output
-    ------
-    new_img : img
+    Returns:
+        new_img: La imagen (matríz) con los filtros aplicados.
     '''
-    gray_example = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-    _, mask = cv2.threshold(gray_example, 120, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    mask_inv = cv2.bitwise_not(mask) 
-    new_img = cv2.bitwise_and(imagen,imagen,mask = mask_inv)
-    return new_img
+    try:
+        gray_example = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(gray_example, 120, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        mask_inv = cv2.bitwise_not(mask) 
+        new_img = cv2.bitwise_and(imagen,imagen,mask = mask_inv)
+
+        return new_img
+
+    except:
+        print(f'El formato {imagen} no es el adecuado. Revisa la descripción de la función.')
 
 def Mejor_PCA_DecissionTree_Regression(X_train, X_test, y_train, y_test, metric, 
                                         list_maxComponents, list_maxDepth, list_maxFeatures):
