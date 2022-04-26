6ab21820d37e9bfe49eed0aee47f0378abdfe854
oscluna <oscarvlc98@gmail.com>
2022-04-26 19:12:01 +0200

    -funcion limpieza

--- src/limpieza/limpieza.py
+++ src/limpieza/limpieza.py
@@ -174,3 +174,39 @@ def crear_rentabilidades(Dataframe,Column_precio, nombre_nueva_columna, n):
 
 
     return Dataframe
+
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+import numpy as np
+
+#En alguna ocasión me ha sido favorable agrupar por porcentaje.
+
+def columnascat(df,nombrecolumntarget,split=" "):                                  #Función para tratar variables categoricas en problemas de clasificación binaria. Necesita el nombre de la columna target entrecomillada y viene por default que el split se haga con un espacio (split=" ").
+    le=LabelEncoder()
+    for i in df.columns:                                                           #Recorremos las columnas en busca de variables categoricas
+        if df[i].dtype==("object" or str):
+            print(i)
+            if (len(set(df[i]))<=(len(df)/20)):                                    #Miramos si hay, por lo menos,5 variables diferentes por cada 100 registros 
+                df[i]=le.fit_transform(df[i])                                      #Si los hay, hacemos un label encoder
+                for q in set(df[i]):                                               #Recorremos las diferentes variables 
+                    x=df[df[i]==q]
+                    porcentaje= ((len(x[x[nombrecolumntarget]==1]))*100)/(len(x))  #Se relaciona la variable con el target y se saca el porcentaje de probabilidad
+                    mask=(df[i]==q)
+                    df.loc[mask,i]=porcentaje                                      #Se aplica una mascara para cambiar cada variable por su porcentaje de probabilidad
+            
+            else:
+                try:
+                    data=df[i].str.split(split)                                     #Si no tenemos minimo 5 variables por cada 100 registros, intentamos dividir las diferentes variables. Util cuando son marcas de coche, procesadores,etc.
+                    df["New"]=data[0]
+
+                    if (len(set(df["New"]))<=(len(df)/20)):                         #Miramos ahora si tenemos 5 variables por cada 100 registros. Si es asi, repetimos el proceso de transformación y agrupación por porcentaje         
+                        df[i]=le.fit_transform(df[i])
+                        for q in set(df[i]):
+                            x=df[df[i]==q]
+                            porcentaje= ((len(x[x[nombrecolumntarget]==1]))*100)/(len(x))
+                            mask=(df[i]==q)
+                            df.loc[mask,i]=porcentaje
+                except:
+                    print("No se puede dividir")
+        else:
+            print("No es una variable categorica")
