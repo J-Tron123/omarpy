8b09bd798ed80dea61db65aa9625707889ff6642
Francisco Palá <francisco.palab@gmail.com>
2022-04-26 16:27:30 +0200

    added func circ_distance

--- src/limpieza/limpieza.py
+++ src/limpieza/limpieza.py
@@ -15,3 +15,28 @@ def num_describe(data_in):
     data_out.insert(0,'sparsity', (data_in==0).sum()/len(data_in))
     data_out.insert(0,'nulls', (data_in.isna()).sum()/len(data_in))
     return data_out
+
+def circ_distance(value_1, value_2, low, high):
+    """Returns distance bweteen two cyclical values
+
+    Args:
+        value_1 (int,float): first value
+        value_2 (int,float): second value
+        low (int,float): _description_
+        high (int,float): _description_
+
+    Returns:
+        float: distance between two values
+    """
+    # minmax scale to 0-2pi rad
+    value_1_rad = ((value_1-low)/(high-low))*(2*np.pi)
+    value_2_rad = ((value_2-low)/(high-low))*(2*np.pi)
+    # sin and cos for coordinates in the unit circle 
+    sin_value_1, cos_value_1 = np.sin(value_1_rad), np.cos(value_1_rad)
+    sin_value_2, cos_value_2 = np.sin(value_2_rad), np.cos(value_2_rad)
+    # dot product is the arccos of alpha
+    angle = np.arccos(np.dot([cos_value_1, sin_value_1],[cos_value_2, sin_value_2]))
+    # convert back to initial units
+    angle = angle*(high-low)/(2*np.pi) + low
+    # return distance
+    return round(angle,3)
