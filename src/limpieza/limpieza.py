17849c622beedd60f1e83c182c74bc208067b92a
Lucy <lucy.95cv@gmail.com>
2022-04-26 16:31:59 +0200

    funct read_images

--- src/limpieza/limpieza.py
+++ src/limpieza/limpieza.py
@@ -1,3 +1,7 @@
+import os
+import cv2
+import numpy as np
+
 def num_describe(data_in):
     """returns a better vesion of describe
 
@@ -15,3 +19,27 @@ def num_describe(data_in):
     data_out.insert(0,'sparsity', (data_in==0).sum()/len(data_in))
     data_out.insert(0,'nulls', (data_in.isna()).sum()/len(data_in))
     return data_out
+
+
+def read_images(path,size,filter=None):
+    '''
+    Función para cargar las imágenes en un array. 
+
+    Parameters
+    ----------
+    path : str
+    size : tuple
+    filter : funct (Default = None)
+
+    Output
+    ------
+    X : array
+    '''
+    X = []
+    for img in os.listdir(path):
+        image = cv2.imread(path+"/"+img)
+        if filter !=None:
+            image = filter(image)
+        smallimage = cv2.resize(image, size)
+        X.append(smallimage)
+    return np.array(X)
