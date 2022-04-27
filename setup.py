007a2c37fb9e68f6977a8ae6d942af675702913b
Francisco Palá <francisco.palab@gmail.com>
2022-04-27 21:54:05 +0200

    fixed readme

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="omarpy",
-    version="0.0.4",
+    version="0.0.5",
+    long_description = long_description
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
