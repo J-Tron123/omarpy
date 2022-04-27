2e38edd9d339311540655349f76f46cb9f023353
Francisco Palá <francisco.palab@gmail.com>
2022-04-27 19:07:51 +0200

    a

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="omarpy",
     version="0.0.1",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     package_dir={'':'src'},
     packages=find_packages(where="src"),
     install_requires =[
