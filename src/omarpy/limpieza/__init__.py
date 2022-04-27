ff82585cea9bc445a08bb8d2a601b7ca9afdac79
Francisco Palá <francisco.palab@gmail.com>
2022-04-27 21:54:51 +0200

    changed version

--- src/omarpy/limpieza/__init__.py
+++ src/omarpy/limpieza/__init__.py
@@ -1,22 +1,3 @@
 """El modulo :mod:omarpy.limpieza incluye funciones que ayudan con la limpieza de datos."""
 
-<<<<<<< HEAD
-from .functions import (
-    num_describe,
-    read_images,
-    circ_distance,
-    inf_as_nan,
-    mice_impute_nans,
-    remove_units,
-    convertidor_español,
-    convertidor_ingles,
-    normalize, 
-    crear_rentabilidades, 
-    columnascat,
-    beautifull_scrap,
-    drop_missings,
-    file_sorter
-)
-=======
 from .functions import *
->>>>>>> ca4af9129d9e0c5c8e01f52d186a5e330c66ba7e
