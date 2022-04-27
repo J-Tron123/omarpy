25cea1ac51a0fd4b883d65d04416477881fd1d1f
Francisco Palá <francisco.palab@gmail.com>
2022-04-27 23:25:41 +0200

    fixed inits

--- src/omarpy/machine_learning/__init__.py
+++ src/omarpy/machine_learning/__init__.py
@@ -1,31 +1,3 @@
 """El modulo :mod:omarpy.machine_learning incluye funciones que ayudan con el entrenamiento de modelos predictivos de machine learning. """
 
-<<<<<<< HEAD
-from .functions import (
-Mejor_PCA_DecissionTree_Regression,
-Mejor_PCA_RandomForest_Regression,
-Mejor_PCA_XGB_Regression,
-scaler,
-prediction,
-class_results,
-binary_class_metrics,
-precision_recall_AUC,
-load_model,
-XgBoost_X_y,
-optunaXGBOOST,
-omar,
-scores,
-similarity_index,
-step_axis,
-percentil,
-metodo_iqr,
-metodo_std,
-DF_Feature_importance,
-preprocess_reviews,
-remove_stop_words,
-sweet_table,
-check_optimizadores)
-
-=======
 from .functions import *
->>>>>>> ca4af9129d9e0c5c8e01f52d186a5e330c66ba7e
--- src/omarpy/visualizacion/__init__.py
+++ src/omarpy/visualizacion/__init__.py
@@ -2,28 +2,4 @@
 El modulo :mod:`omarpy.visualizacion` incluye funciones que ayudan con la visualización de datos. 
 """
 
-<<<<<<< HEAD
-from .functions import (
-    graf_bar_horizon,
-    move_spines,
-    plot_pairplot,
-    stacked_bar_plot,
-    y,
-    h,
-    num_plot,
-    bar_hor,
-    plot_bar_chart_with_numbers_y, 
-    plot_boxplot, 
-    data_report,
-    grafico,
-    scat_log_visualize,
-    heatmap,
-    graphs_sub,
-    sweet_heatmap, 
-    sweet_pie,
-    sweet_cloud,
-    plot_numerical_huetarget,
-)
-=======
 from .functions import *
->>>>>>> ca4af9129d9e0c5c8e01f52d186a5e330c66ba7e
